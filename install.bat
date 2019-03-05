e34c4a7dfca41de7a621c68ac057a069c89efdcc
Zsolt Kozaroczy <kiskoza@gmail.com>
2019-03-05 09:22:16 +0100

    Fix typo

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 (
-  echo del /F /Q "C:\Users\HimzoVM-10\Desktop\bernina"
-  echo rmdir "C:\Users\HimzoVM-10\Desktop\bernina"
+  echo del /F /Q "C:\Users\HimzoVM-10\Desktop\bernina8"
+  echo rmdir "C:\Users\HimzoVM-10\Desktop\bernina8"
   echo del install.bat
   echo del cleanup.bat
 ) > cleanup.bat
